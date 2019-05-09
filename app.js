a65811d5d94296641d26409b55d2e86b5af94444
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-09 13:02:32 +0000

    Added function to get weather when DOM Content is loaded

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 
 // Init Weather object
 
-const weather = new Weather('London', 'uk');
+const weather = new Weather('Sibiu', 'ro');
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', fetchWeather);
+
+// weather.changeLocation('London', 'uk');
 
 
-weather.getWeather()
-    .then(data =>  {
-        console.log(data);
-    })
-    .catch(err => console.log(err));
+function fetchWeather() {
+    weather.getWeather()
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => console.log(err));
+}
