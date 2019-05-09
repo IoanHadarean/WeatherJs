9d82893e56a521ee60103183cbc45e1ded1ac4ea
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-09 12:39:58 +0000

    Added weather class and fetch request for WeatherAPI

---
+++ weather.js
@@ -0,0 +1,19 @@
+/* global fetch */
+
+class Weather {
+    constructor(city, countryCode) {
+        this.apiKey = '4c8f9914e188ba211a9ceef14a04b60e';
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+    
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`);
+    
+        const resData =  await response.json();
+        
+        return resData;
+        
+    }
+}
